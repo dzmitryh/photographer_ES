4882708c6b4f7febd0af0f19c5aa371f27b9ad9b
Dzmitry <dzmitry.hubin@gmail.com>
2013-11-04 22:12:31 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhotographerEs::Application.config.secret_key_base = 'b414300a516e4a57b10c57e4165d33976258cab365bc8feac116a00921cb332e028d2061046e06cc376f34cdba1e128fa7714c5e2046a06eb4ad3d062053843e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PhotographerEs::Application.config.session_store :cookie_store, key: '_photographer_ES_session'
